eaac9342798ed58f9093e9c9d9ff2c92a6ac2f53
cemmakkaya <cem.akkaya@bbzbl-it.ch>
2024-10-27 13:50:23 +0100

    Fix Bug | V2 finished

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
      * @param args Kommandozeilenargumente (werden nicht verwendet)
      */
     public static void main(String[] args) {
-        DataLoader loader = new DataLoader("/Users/cemakkaya/IdeaProjects/fahrgaesteBVB/src/100075.json");
+        DataLoader loader = new DataLoader("src/100075.json");
         List<PassengerData> data = loader.loadData();
 
         if (data == null || data.isEmpty()) {
@@ -30,15 +30,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("------------------Version 2------------------");
-            System.out.println("");
+            System.out.println("\n-----------------Version 1-------------------");
             System.out.println("Geben Sie einen Zeitraum ein (Jahr, Quartal, Monat oder Woche):");
             System.out.println("Oder geben Sie 'exit' ein, um das Programm zu beenden.");
-            System.out.println("");
             System.out.println("---------------------------------------------");
-            System.out.println("");
-            System.out.println("Beispiel: '2020', '2020-Q1', '2020-02', '2020-W06'");
-            System.out.println("Datensatz geht von 2020-02 bis 2024-08.");
+            System.out.println("Formate:");
+            System.out.println("- Jahr: YYYY (z.B. 2020)");
+            System.out.println("- Quartal: YYYY-QN (z.B. 2020-Q1)");
+            System.out.println("- Monat: YYYY-MM (z.B. 2020-02)");
+            System.out.println("- Woche: YYYY-WNN (z.B. 2020-W06)");
+            System.out.println("Datensatz geht von 2020-02 bis 2024-08");
+            System.out.println("---------------------------------------------");
 
 
             String input = scanner.nextLine().trim();
